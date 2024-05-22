4775c053ad0c8b6912b1be337abb493acd3ad262
karyne0 <karynebarbosa4393@gmail.com>
2024-05-22 17:09:12 -0300

    commit 1

--- cadastro.php
+++ cadastro.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar Usuários</title>
+    <title>Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -49,7 +49,7 @@ if(isset($_POST['submit']))
         <label for="floatingInput">Criar Senha</label>
     </div>
     <br>
-    <button type="submit" name="submit" id="submit" class="btn btn-primary"><a href="login.php">enviar</a></button>
+    <button type="submit" name="submit" id="submit" class="btn btn-primary">enviar</button>
 </form>
 </main>
 </body>
--- sistema.php
+++ sistema.php
@@ -7,7 +7,6 @@ if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true
     unset($_SESSION['senha']);
     header('Location: login.php');
 }
-$logado = $_SESSION['email'];
 ?>
 
 <!DOCTYPE html>

3bd032c1d7212f6f3e2beb92141854c598f93614
karyne0 <karynebarbosa4393@gmail.com>
2024-05-23 14:57:14 -0300

    commit 2

--- cadastro.php
+++ cadastro.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <title>cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -49,8 +49,12 @@ if(isset($_POST['submit']))
         <label for="floatingInput">Criar Senha</label>
     </div>
     <br>
-    <button type="submit" name="submit" id="submit" class="btn btn-primary">enviar</button>
-</form>
+    <div id="btn">
+        <button type="submit" name="submit" id="submit" class="btn btn-primary">enviar</button>
+        <button type="submit" name="submit" id="submit" class="btn btn-primary"><a href="login.php">logar</a></button>
+
+    </div>
+    </form>
 </main>
 </body>
 </html>

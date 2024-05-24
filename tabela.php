9bae782d535809509c892af6844c5b0e303626b8
karyne0 <karynebarbosa4393@gmail.com>
2024-05-24 17:08:09 -0300

    commit

---
+++ tabela.php
@@ -0,0 +1,17 @@
+<?php
+$sql = "SELECT * FROM usuarios";
+
+$result = $result->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios-lista</title>
+</head>
+<body>
+    
+</body>
+</html>

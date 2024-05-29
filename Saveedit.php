7be9c0c89ffba63babda18e3886f2c563d9a6087
karyne0 <karynebarbosa4393@gmail.com>
2024-05-29 17:03:14 -0300

    commit edit table

---
+++ Saveedit.php
@@ -0,0 +1,16 @@
+<?php
+include('config.php');
+
+if(isset($_POST['update']))
+{
+    $nome =  $_POST['nome'];
+    $email =  $_POST['email'];
+    $senha =  $_POST['senha'];
+
+    $sqlUpdate = "UPDATE usuarios SET nome='$nome', email='$email', senha='$senha' WHERE id='$id'";
+
+    $result = $conexao->query($sqlUpdate);
+
+}
+header('Location: tabela.php');
+?>
